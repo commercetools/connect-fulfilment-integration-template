de04ebc0374ff79340ae0a695bd38d1ee1cd68c8
praveenkumarct <praveen.kumar@commercetools.com>
2024-05-22 11:37:55 +0200

    Fix lint issues

--- inventory-import/src/controllers/inventory.import.controller.js
+++ inventory-import/src/controllers/inventory.import.controller.js
@@ -1,18 +1,24 @@
 import _ from 'lodash';
-import {logger} from '../utils/logger.utils.js';
+import { logger } from '../utils/logger.utils.js';
 import {
   HTTP_STATUS_BAD_REQUEST,
   HTTP_STATUS_SERVER_ERROR,
   HTTP_STATUS_SUCCESS_ACCEPTED,
 } from '../constants/http.status.constants.js';
 import CustomError from '../errors/custom.error.js';
-import {createInventoryEntry, getInventoryEntryByKey, updateInventoryEntry} from "../clients/query.client.js";
+import {
+  createInventoryEntry,
+  getInventoryEntryByKey,
+  updateInventoryEntry,
+} from '../clients/query.client.js';
 
 async function syncInventoryEntry(key, inventoryRequest, response) {
   let inventoryToBeSynced = await getInventoryEntryByKey(key);
 
-  if( inventoryToBeSynced ) {
-    logger.info(`Inventory found: ${inventoryToBeSynced.key}, updating the existing inventory.`);
+  if (inventoryToBeSynced) {
+    logger.info(
+      `Inventory found: ${inventoryToBeSynced.key}, updating the existing inventory.`
+    );
 
     try {
       await updateInventoryEntry(inventoryToBeSynced);
@@ -26,28 +32,28 @@ async function syncInventoryEntry(key, inventoryRequest, response) {
 
     await createInventoryEntry(inventoryRequest).catch((error) => {
       throw new CustomError(
-          HTTP_STATUS_BAD_REQUEST,
-          `Bad request: ${error.message}`,
-          error
+        HTTP_STATUS_BAD_REQUEST,
+        `Bad request: ${error.message}`,
+        error
       );
     });
   }
   logger.info(`Inventory(s) has been added/updated.`);
 
-  return response.status(HTTP_STATUS_SUCCESS_ACCEPTED).send(
-      { message: "successfully created/updated" }
-  );
+  return response
+    .status(HTTP_STATUS_SUCCESS_ACCEPTED)
+    .send({ message: 'successfully created/updated' });
 }
 
 function getInvalidRequestResponse(response) {
   return response
-      .status(HTTP_STATUS_BAD_REQUEST)
-      .send(
-          new CustomError(
-              HTTP_STATUS_BAD_REQUEST,
-              'Missing Inventory information in the request body.'
-          )
-      );
+    .status(HTTP_STATUS_BAD_REQUEST)
+    .send(
+      new CustomError(
+        HTTP_STATUS_BAD_REQUEST,
+        'Missing Inventory information in the request body.'
+      )
+    );
 }
 
 export const inventoryHandler = async (request, response) => {
@@ -57,12 +63,13 @@ export const inventoryHandler = async (request, response) => {
   }
 
   let key = inventoryRequest.key;
-  if(_.isEmpty(key)) {
+  if (_.isEmpty(key)) {
     const sku = inventoryRequest.sku;
     const supplyChannel = inventoryRequest.supplyChannel;
     if (!_.isEmpty(sku) && !_.isEmpty(supplyChannel)) {
-      key = _.isEmpty(supplyChannel.id) ? `${sku}+${inventoryRequest.supplyChannel.id}}` :
-          `${sku}+${inventoryRequest.supplyChannel.key}}`
+      key = _.isEmpty(supplyChannel.id)
+        ? `${sku}+${inventoryRequest.supplyChannel.id}}`
+        : `${sku}+${inventoryRequest.supplyChannel.key}}`;
     } else {
       return getInvalidRequestResponse(response);
     }
